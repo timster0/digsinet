41f1a54785eb84104c850995a5508b86551ff795
Tim Benson <tim.benson@deutschebahn.com>
2025-12-04 14:57:39 +0100

    wip: NATS client implementation

--- event/nats.py
+++ event/nats.py
@@ -1,25 +1,38 @@
 from logging import Logger
-from typing import List
+from typing import Dict, List
 from event.eventbroker import EventBroker
 from config.nats import NatsSettings
+from message.message import Message
+from message.nats import NatsMessage
+import nats_sync
+import json
 
 class NatsClient(EventBroker):
     def __init__(self, config: NatsSettings, channels: List[str], logger: Logger):
         super().__init__(config, channels, logger)
-        # TODO Implement NATS client
-        pass
+        self.client = nats_sync.connect(f"nats://{config.host}:{config.port}")
+        # There is no need to initialize topics in NATS
+        self.channels: List[str] = channels
+        self.logger: Logger = logger
+        self.subscribers: Dict[str, nats_sync.nats_ctx.NATSSub] = dict()
 
     def publish(self, channel: str, data):
-        # TODO Implement NATS publish
-        pass
+        if channel not in self.channels:
+            self.logger.warning(f"NATS Topic {channel} is an unknown channel")
+        serialized_data: str = json.dumps(data, default=lambda x: "<not serializable>")
+        self.logger.info(f"Publishing message to NATS topic {channel}: {serialized_data}")
+        self.client.publish(channel, bytes(serialized_data))
 
-    def poll(self, consumer, timeout):
+    def poll(self, consumer, timeout) -> Message:
+        self.client
         # TODO Implement NATS poll
         pass
 
     def subscribe(self, channel: str, group_id: str = None):
-        # TODO Implement NATS subscribe
-        pass
+        if channel not in self.subscribers.keys():
+            self.client.subscribe(channel)
+        else:
+            self.logger.warning(f"Tried to subscribe to channel with active subscription: {channel}")
 
     def get_sibling_channels(self):
         return self.topics
